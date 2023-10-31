6885a9613beebe3baea3ebba0de087887e7fed97
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-10-31 16:29:13 -0400

    Created ZipLoc struct and get_coords fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use ureq;
+use std::env;
+use serde::Deserialize;
+
+#[derive(Clone, Debug, Deserialize)]
+struct ZipLoc {
+    zip: String,
+    name: String,
+    lat: String,
+    lon: String,
+    country: String,
+}
+
+fn get_coords_zipcode(zip: String, country: String, apikey: String) -> Result<ZipLoc, ureq::Error> {
+    let url: String = format!("http://api.openweathermap.org/geo/1.0/zip?zip={zip},{country}&appid={apikey}");
+    let response: ZipLoc = ureq::get(&url).call()?.into_json()?;
+    Ok(response)
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
