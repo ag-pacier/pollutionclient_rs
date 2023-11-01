88808f239e7b8259e403571a7498653249ee2b30
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-10-31 22:01:32 -0400

    Implemented config
    Includes new config, setting values, getting the key and parsing needed environmental variables

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,53 @@ use ureq;
 use std::env;
 use serde::Deserialize;
 
+#[derive(Clone, Debug)]
+struct Config {
+    apikey: Option<String>,
+    location: Option<ZipLoc>,
+}
+
+impl Config {
+    fn new() -> Config {
+        Config { apikey: None, location: None }
+    }
+    fn set_loc(&mut self, new_loc: ZipLoc) -> () {
+        self.location = Some(new_loc);
+    }
+    fn set_key(&mut self, new_key: String) -> () {
+        self.apikey = Some(new_key);
+    }
+    fn get_key(&self) -> String {
+        match &self.apikey {
+            Some(key) => key.to_owned(),
+            None => "None".to_string(),
+        }
+    }
+    fn parse_env() -> Result<Config, ureq::Error> {
+        let mut currentConfig: Config = Config::new();
+        let new_api_key: Option<String> = match env::var("OPENWEATHER_API_KEY") {
+            Ok(key) => Some(key),
+            Err(_) => None,
+        };
+        if new_api_key.is_some() {
+            currentConfig.set_key(new_api_key.unwrap());
+        };
+        let zip_code: Option<String> = match env::var("OPENWEATHER_POLL_ZIP") {
+            Ok(set_zip) => Some(set_zip),
+            Err(_) => None,
+        };
+        if zip_code.is_some() {
+            let country: String = match env::var("OPENWEATHER_POLL_COUNTRY") {
+                Ok(set_country) => set_country,
+                Err(_) => "US".to_string(),
+            };
+            let env_location = get_coords_zipcode(zip_code.unwrap(), country, currentConfig.get_key())?;
+            currentConfig.location = Some(env_location);
+        }
+        Ok(currentConfig)
+    }
+}
+
 #[derive(Clone, Debug, Deserialize)]
 struct ZipLoc {
     zip: String,
